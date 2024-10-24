011a577b63b7c4b1deecdf3cb60828b9545ef2e6
Jun Young Kim <145519413+JunKimmmy@users.noreply.github.com>
2024-10-23 23:40:08 -0400

    Update script.js

--- proj/script.js
+++ proj/script.js
@@ -123,3 +123,18 @@ document.addEventListener("DOMContentLoaded", function() {
         slider.children[currentSlide].style.display = "block";
     }
 });
+
+import { defineStackbitConfig } from '@stackbit/types'
+
+export default defineStackbitConfig({
+  stackbitVersion: '~0.6.0',
+  ssgName: 'gatsby',
+  nodeVersion: '18', 
+  experimental: { 
+    ssg: { 
+      logPatterns: { 
+        up: ['success onPreInit'], 
+      }, 
+    }, 
+  },
+})
